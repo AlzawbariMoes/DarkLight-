b283096f0ae64504f47e927f29dbd8360a024c1c
Alzawbari Mohammed <38676494+AlzawbariMoes@users.noreply.github.com>
2020-04-24 19:41:24 +0300

    The flappy bird game

---
+++ Assets/BGMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMover : MonoBehaviour {
+
+
+	float speed = -2f;
+	void FixUpdate()
+	{
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		
+	}
+}
